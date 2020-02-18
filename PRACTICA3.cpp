a1d7c7562ecff49d345ad46e6c348cd5f1dbaf44
gustavo1904391 <60495698+gustavo1904391@users.noreply.github.com>
2020-02-18 16:45:29 -0600

    VIVIENDO AL LIMITE 2.1

---
+++ PRACTICA3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv []) {
+	char a;
+	printf("TECLEA LO QUE QUIERAS BRO,OJO SOLO UNA VEZ :v\n");
+	a=getchar();
+	int b=(int)a;
+	if(48<=b && b<=57){
+		printf("ES UN NUMERO");
+	}
+	else{
+		if(65<=b && b<=90){
+			printf("ES UNA MAYUSCULA");
+		}
+		else{
+			if(97<=b && b<=122){
+				printf("ES UNA MINUSCULA");
+			}
+		
+		else{
+			printf("ES UN CARACTER");}
+		}
+	}
+	return 0;
+}
